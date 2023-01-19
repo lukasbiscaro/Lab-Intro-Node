a5dc7a5495927b74c7f559bd7a1190d5580732c2
Lukas de Oliveira Biscaro <lukas.biscaro@hotmail.com>
2023-01-19 08:21:41 -0300

    Done + Bonus.

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a - b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos > this.items.length) {
+      throw new Error('OutOfBounds')
+    } else return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.items) {
+      return this.items.reduce((a, b) => Math.max(a, b))
+    } else throw new Error('EmptySortedList')
+  }
 
-  min() {}
+  min() {
+    if (this.items) {
+      return this.items.reduce((a, b) => Math.min(a, b))
+    } else throw new Error('EmptySortedList')
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList')
+    } else return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
